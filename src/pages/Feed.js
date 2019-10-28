ee4c2058bdc57abd7f149adb26cb0c6d03d76bf7
Nelio Alves <jefferson2112@hotmail.com>
2019-10-28 15:19:28 -0300

    Add lib axios

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import './Feed.css';
 
@@ -8,64 +9,48 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+  state = {
+    feed: []
+  }
+
+  async componentDidMount() {
+    const response = await api.get('posts');
+
+    this.setState({ feed: response.data })
+  }
+
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Diego Fernandes</span>
-              <span className="place">Rio do Sul</span>
-            </div>
-
-            <img src={more} alt="Mais" />
-          </header>
-
-          <img src="http://localhost:3333/files/carro2.jpg" alt=""/>
-
-          <footer>
-            <div classname="actions">
-              <img src={like} alt=""/>
-              <img src={comment} alt=""/>
-              <img src={send} alt=""/>
-            </div>
-
-            <strong>900 curtidas</strong>
-
-            <p>
-              Um Post foda da RockeSeat
-              <span>#react #bootcamp</span>
-            </p>
-          </footer>
-        </article>
-
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Diego Fernandes</span>
-              <span className="place">Rio do Sul</span>
-            </div>
-
-            <img src={more} alt="Mais" />
-          </header>
-
-          <img src="http://localhost:3333/files/carro2.jpg" alt=""/>
-
-          <footer>
-            <div classname="actions">
-              <img src={like} alt=""/>
-              <img src={comment} alt=""/>
-              <img src={send} alt=""/>
-            </div>
-
-            <strong>900 curtidas</strong>
-
-            <p>
-              Um Post foda da RockeSeat
-              <span>#react #bootcamp</span>
-            </p>
-          </footer>
-        </article>
+        { this.state.feed.map(post => (
+          <article>
+            <header>
+              <div className="user-info">
+                <span>{ post.author }</span>
+                  <span className="place">{ post.place }</span>
+              </div>
+        
+              <img src={more} alt="Mais" />
+            </header>
+        
+            <img src={ `http://localhost:3333/files/${post.image}` } alt=""/>
+        
+            <footer>
+              <div classname="actions">
+                <img src={like} alt=""/>
+                <img src={comment} alt=""/>
+                <img src={send} alt=""/>
+              </div>
+      
+              <strong>{ post.likes }</strong>
+        
+              <p>
+                { post.description }
+                <span>{ post.hashtags }</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     )
   }
